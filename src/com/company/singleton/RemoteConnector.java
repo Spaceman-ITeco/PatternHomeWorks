cb95a5f8b956ab7a6e65dd53cb43ce1234d154d9
@poluhin <poluhin@i-teco.ru>
2022-07-14 11:44:14 +0300

    Singleton Method Pattern Homework

--- src/com/company/singleton/RemoteConnector.java
+++ src/com/company/singleton/RemoteConnector.java
@@ -1,14 +1,38 @@
 package com.company.singleton;
 
-public class RemoteConnector {
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+public class RemoteConnector implements Externalizable {
     private static RemoteConnector remoteConnector;
+    private boolean connected;
     private String address;
-    private RemoteConnector(String address)
-    {this.address=address;}
-    public static synchronized void connect(String address)
+
+    private RemoteConnector()
+    {}
+    public synchronized void connect(String address)
     {
-        if (remoteConnector == null)
-        {remoteConnector = new RemoteConnector(address);
+        if (connected)
+        {return;
         }
+        connected = true;
+        this.connect(this.address=address);
+    }
+    public static synchronized RemoteConnector getInstance()
+    { if (remoteConnector == null)
+    {remoteConnector = new RemoteConnector();
+    }
+    return remoteConnector;
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+
+    }
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+
     }
 }
